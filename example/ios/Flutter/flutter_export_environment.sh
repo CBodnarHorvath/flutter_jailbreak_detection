5ecb9b1355bac0e9abfafa5a29d0ad6876cdb66e
CBodnarHorvath <cbodnarhorvath@palo-it.com>
2022-09-29 11:08:25 +0800

    added www to jitpack

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/jeroentrappers/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/jeroentrappers/Developer/github/flutter_jailbreak_detection/example"
+export "FLUTTER_ROOT=/Users/charlesbodnarhorvath/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/charlesbodnarhorvath/Documents/GitHub/flutter_jailbreak_detection/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
